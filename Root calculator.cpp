a3650508a6645dea1725f1abacc9eecd6e8fc68f
Erdemhan <erdemhan@users.noreply.github.com>
2018-10-12 01:01:20 +0300

    Main Release
    Main release files added.

---
+++ Root calculator.cpp
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+// This program finds roots of ax^2+bx+c ( only real number roots)
+
+int main (void){
+	
+	printf("This Program finds roots of ax^2+bx+c (Only real number roots)\n\n");
+	
+		double a,b,c,delt,deltq,root1,root2;
+		
+		
+	printf("Please enter the 'a' \n");		scanf("%lf",&a);
+	printf("Please enter the 'b' \n");		scanf("%lf",&b);
+	printf("Please enter the 'c' \n");		scanf("%lf",&c);
+
+	delt=(b*b-4*a*c);
+	
+	if(delt<0){
+	printf("There is no real number root\n"); 
+	}
+	else if(delt==0){
+		
+	root1=(-b+sqrt(delt))/(2*a);
+	printf("There are double roots: %lf\n",root1);
+	}
+	else{
+	
+	root1=(-b+sqrt(delt))/(2*a);
+	root2=(-b-sqrt(delt))/(2*a);
+	printf("There are two roots: %lf , %lf\n",root1,root2);
+}
+	system("PAUSE");
+	
+	
+}
